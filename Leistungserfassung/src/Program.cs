b2680d952b3de9a6d3c3d47d8e9609fd22c4c5d0
mariekart42 <marie.a.mensing@gmail.com>
2024-05-06 17:15:46 +0200

    created postgres db

--- Leistungserfassung/src/Program.cs
+++ Leistungserfassung/src/Program.cs
@@ -2,5 +2,19 @@
 
 Console.WriteLine("Hello, World!");
 
-//ahh
-Console.WriteLine("init");
+// string[] test_data = ['GEBONR': 'V01A', 'ABRCODEIDBER': '¤E10¤', 'UIDBEAIDBER': '¤EFEG¤', 'OPWBTRIDBER': '¤E90.0001¤',
+//    'STATUS': 'E'},
+//   ['GEBONR': 'V01A', 'ABRCODEIDBER': '¤E10¤', 'UIDBEAIDBER': '¤EFEG¤', 'OPWBTRIDBER': '¤E91.0000¤',
+//    'STATUS': 'E'];
+
+
+// test_data = [
+//  {'GEBONR': 'V01A', 'ABRCODEIDBER': '¤E10¤', 'UIDBEAIDBER': '¤EFEG¤', 'OPWBTRIDBER': '¤E90.0001¤',
+//   'STATUS': 'E'},
+//  {'GEBONR': 'V01A', 'ABRCODEIDBER': '¤E10¤', 'UIDBEAIDBER': '¤EFEG¤', 'OPWBTRIDBER': '¤E91.0000¤',
+//   'STATUS': 'E'},
+// ]
+// bea = 'FEG'
+//
+// lol = get_gesamt_leistung(test_data, bea)
+// print('RESULT: ', lol)
